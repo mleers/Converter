129bdb0511d9bca9fbbbaffbf006a9c0d6a61b3e
mleers <mleers4@gmail.com>
2022-10-01 17:31:31 -0700

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Mitch on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
